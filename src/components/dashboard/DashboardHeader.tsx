bc007dcbb3bc6c44e272ef9a93d9c10162042cc7
emonkhan500 <khdilo47@gmail.com>
2026-03-09 16:18:10 +0600

    complete

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -39,11 +39,11 @@ const DashboardHeader = ({ setSidebarOpen }: DashboardHeaderProps) => {
               name="search"
               placeholder="Search"
               aria-label="Search"
-              className="col-start-1 row-start-1 block size-full border border-border-gray px-18 text-base text-sidebar-text outline-hidden placeholder:text-sidebar-text sm:text-sm/6 rounded-xl bg-sidebar-bg "
+              className="col-start-1 row-start-1 block size-full border border-border-gray px-10 md:px-18 text-base text-sidebar-text outline-hidden placeholder:text-sidebar-text sm:text-sm/6 rounded-xl bg-sidebar-bg "
             />
             <MagnifyingGlassIcon
               aria-hidden="true"
-              className="pointer-events-none col-start-1 row-start-1 size-5 self-center text-sidebar-text ml-8"
+              className="pointer-events-none col-start-1 row-start-1 size-5 self-center text-sidebar-text ml-4 md:ml-8"
             />
           </form>
           <div className="flex items-center gap-x-4 lg:gap-x-6">
@@ -78,27 +78,8 @@ const DashboardHeader = ({ setSidebarOpen }: DashboardHeaderProps) => {
                   >
                     Tom Cook
                   </span>
-                  <ChevronDownIcon
-                    aria-hidden="true"
-                    className="ml-2 size-5 text-gray-500"
-                  />
                 </span>
               </MenuButton>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-sidebar-bg py-2 outline-1 -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-              >
-                {userNavigation.map((item) => (
-                  <MenuItem key={item.name}>
-                    <a
-                      href={item.href}
-                      className="block px-3 py-1 text-sm/6 text-sidebar-text hover:text-custom-green data-focus:bg-white/5 data-focus:outline-hidden hover:bg-sidebar-bg"
-                    >
-                      {item.name}
-                    </a>
-                  </MenuItem>
-                ))}
-              </MenuItems>
             </Menu>
           </div>
         </div>
